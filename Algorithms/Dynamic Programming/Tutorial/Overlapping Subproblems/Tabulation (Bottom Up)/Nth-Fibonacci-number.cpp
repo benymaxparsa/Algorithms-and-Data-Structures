186a793a821aac58411bc77280148232a215c5f5
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-03-26 18:03:29 +0430

    fibo code for tabulation method

--- Algorithms/Dynamic Programming/Tutorial/Overlapping Subproblems/Tabulation (Bottom Up)/Nth-Fibonacci-number.cpp
+++ Algorithms/Dynamic Programming/Tutorial/Overlapping Subproblems/Tabulation (Bottom Up)/Nth-Fibonacci-number.cpp
@@ -1,1 +1,27 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+int Fib(int n)
+{
+    int table[n+1];
+
+    table[0] = 0;
+    table[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        table[i] = table[i - 1] + table[i - 2];
+    }
+
+    return table[n];
+
+}
+
+int main()
+{
+    int n = 5;
+
+    cout << Fib(5)<<endl;
+
+    return 0;
+}
